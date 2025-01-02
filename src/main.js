1ff5dfc4de8038dfd05550f1a8f7b5b407ae3902
Jiang Wenjian <wenjian.jiang@thalesgroup.com>
2025-01-02 15:20:46 +0800

    click to drag

--- src/main.js
+++ src/main.js
@@ -1,27 +1,43 @@
-const { invoke } = window.__TAURI__.core;
+let scaleFactor = 1; // Initial scale factor
+const zoomSpeed = 0.1; // Adjust zoom sensitivity
 
-let greetInputEl;
-let greetMsgEl;
+const currentWindow = window.__TAURI__.window.getCurrentWindow(0);
 
-async function greet() {
-  // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-  greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
-}
+console.log(currentWindow);
 
 async function minimizeWindow() {
-  await window.__TAURI__.window.getCurrentWindow().minimize();
+  await currentWindow.minimize();
+}
+
+async function startDragging() {
+  await currentWindow.startDragging();
 }
 
 window.addEventListener("DOMContentLoaded", () => {
-  greetInputEl = document.querySelector("#greet-input");
-  greetMsgEl = document.querySelector("#greet-msg");
-  document.querySelector("#greet-form").addEventListener("submit", (e) => {
-    e.preventDefault();
-    greet();
-  });
   document.addEventListener("keydown", (e) => {
     if (e.key === "Escape") {
       minimizeWindow();
     }
   });
+
+  document.addEventListener("mousedown", (e) => {
+    startDragging();
+  });
+
+  // document.addEventListener('wheel', async (e) => {
+  //   e.preventDefault();
+  
+  //   // Adjust scale factor
+  //   scaleFactor += e.deltaY < 0 ? zoomSpeed : -zoomSpeed;
+  //   scaleFactor = Math.max(0.5, Math.min(3, scaleFactor)); // Limit zoom scale
+  
+  //   // Get current window size
+  //   const { width, height } = await window.__TAURI__.window.innerSize();
+  
+  //   // Set new window size
+  //   window.__TAURI__.window.setSize({
+  //     width: Math.round(width * scaleFactor),
+  //     height: Math.round(height * scaleFactor),
+  //   });
+  // });
 });
